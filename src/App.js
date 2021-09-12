239f82f124acf505f061b4b4cc57bdce17388ed2
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-09-12 15:59:08 -0300

    1.1 - Ciclo de vida

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Twitter from './Twitter';
+
+class App extends Component {
+
+  state = {
+    loading: false,
+    actived: true
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        loading: true
+      })
+    }, 3000)
+  }
+
+  onRemove = () => {
+    this.setState({
+      actived: false
+    })
+  }
+
+  render() {
+
+    const posts = [{
+      title: 'xpto',
+      descriptio: 'foo'
+    },
+    {
+      title: 'xpto',
+      descriptio: 'foo'
+    }]
+
+    return (
+      <div>
+        <button onClick={this.onRemove}>Remover component</button>
+        {this.state.actived && (
+          <Twitter posts={posts} loading={this.state.loading} />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
